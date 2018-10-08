7d3510dadaf113974bc392978fdd3584366d34fc
bora.avramovic15 <bora.avramovic15@gmail.com>
2018-10-08 14:41:57 +0200

    Izmene + MIGRACIJA.

---
+++ RepositoryCore/Migrations/20181008124116_Country_Bora.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RepositoryCore.Migrations
+{
+    public partial class Country_Bora : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CountryId",
+                table: "Companies",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Identifier = table.Column<Guid>(nullable: false),
+                    CreatedById = table.Column<int>(nullable: true),
+                    CompanyId = table.Column<int>(nullable: true),
+                    Active = table.Column<bool>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false),
+                    Code = table.Column<string>(nullable: true),
+                    AlfaCode = table.Column<string>(nullable: true),
+                    NumericCode = table.Column<string>(nullable: true),
+                    Mark = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Countries_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Countries_Users_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_CountryId",
+                table: "Companies",
+                column: "CountryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Countries_CompanyId",
+                table: "Countries",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Countries_CreatedById",
+                table: "Countries",
+                column: "CreatedById");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Companies_Countries_CountryId",
+                table: "Companies",
+                column: "CountryId",
+                principalTable: "Countries",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Companies_Countries_CountryId",
+                table: "Companies");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Companies_CountryId",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "CountryId",
+                table: "Companies");
+        }
+    }
+}
