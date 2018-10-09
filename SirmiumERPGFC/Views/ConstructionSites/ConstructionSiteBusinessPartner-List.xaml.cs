7e6c00c86f23042b8a89be93b01504c594cbb8fe
Strnisa <miroslavstrnisa@yahoo.com>
2018-10-09 10:02:36 +0200

    Forma za firme po gradilistu

---
+++ SirmiumERPGFC/Views/ConstructionSites/ConstructionSiteBusinessPartner-List.xaml.cs
@@ -0,0 +1,105 @@
+﻿using ServiceInterfaces.ViewModels.ConstructionSites;
+using SirmiumERPGFC.Common;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SirmiumERPGFC.Views.ConstructionSites
+{
+    public partial class ConstructionSiteBusinessPartner_List : UserControl, INotifyPropertyChanged
+    {
+        #region ConstructionSitesFromDB
+        private ObservableCollection<ConstructionSiteViewModel> _ConstructionSitesFromDB;
+
+        public ObservableCollection<ConstructionSiteViewModel> ConstructionSitesFromDB
+        {
+            get { return _ConstructionSitesFromDB; }
+            set
+            {
+                if (_ConstructionSitesFromDB != value)
+                {
+                    _ConstructionSitesFromDB = value;
+                    NotifyPropertyChanged("ConstructionSitesFromDB");
+                }
+            }
+        }
+        #endregion
+
+        public ConstructionSiteBusinessPartner_List()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+
+            ConstructionSitesFromDB = new ObservableCollection<ConstructionSiteViewModel>()
+            {
+                new ConstructionSiteViewModel()
+                {
+                    Code = "123",
+                    Name = "SirmiumERP d.o.o."
+                }
+            };
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            FlyoutHelper.OpenFlyout(this, "Firme po gradilištima", 95, new ConstructionSiteBusinessPartner_List_AddEdit());
+        }
+
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnFirstPage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPrevPage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnNextPage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLastPage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #region INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        // This method is called by the Set accessor of each property.
+        // The CallerMemberName attribute that is applied to the optional propertyName
+        // parameter causes the property name of the caller to be substituted as an argument.
+        public void NotifyPropertyChanged(String propertyName) // [CallerMemberName] 
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
