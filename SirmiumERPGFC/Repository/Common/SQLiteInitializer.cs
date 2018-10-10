cf3481faa3809b4bef3e5d0f218c9fdfa320ec2b
milancica <marticmilanka@gmail.com>
2018-10-10 11:55:07 +0200

    Sitne izmene na firmama

--- SirmiumERPGFC/Repository/Common/SQLiteInitializer.cs
+++ SirmiumERPGFC/Repository/Common/SQLiteInitializer.cs
@@ -146,6 +146,34 @@ namespace SirmiumERPGFC.Repository.Common
                     createTable.ExecuteReader();
                     #endregion
 
+                    #region BusinessPartnerOrganizationUnit
+                    if (withTableDrop)
+                    {
+                        try
+                        {
+                            SqliteCommand dropTable = new SqliteCommand("DROP TABLE BusinessPartnerOrganizationUnits", db);
+                            dropTable.ExecuteNonQuery();
+                        }
+                        catch (Exception ex) { }
+                    }
+                    createTable = new SqliteCommand(BusinessPartnerOrganizationUnitSQLiteRepository.BusinessPartnerOrganizationUnitTableCreatePart, db);
+                    createTable.ExecuteReader();
+                    #endregion
+
+                    #region BusinessPartnerPhone
+                    if (withTableDrop)
+                    {
+                        try
+                        {
+                            SqliteCommand dropTable = new SqliteCommand("DROP TABLE BusinessPartnerPhones", db);
+                            dropTable.ExecuteNonQuery();
+                        }
+                        catch (Exception ex) { }
+                    }
+                    createTable = new SqliteCommand(BusinessPartnerPhoneSQLiteRepository.BusinessPartnerPhoneTableCreatePart, db);
+                    createTable.ExecuteReader();
+                    #endregion
+
                     #region BusinessPartnerType
                     if (withTableDrop)
                     {
