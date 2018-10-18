a3b7f476937478667569b14fbf2ed7846fade846
milancica <marticmilanka@gmail.com>
2018-10-18 08:33:17 +0200

    MIGRACIJA

--- RepositoryCore/Migrations/20181018063235_Izmena_Milanka.Designer.cs
+++ RepositoryCore/Migrations/20181018063235_Izmena_Milanka.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RepositoryCore.Context;
 
 namespace RepositoryCore.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181018063235_Izmena_Milanka")]
+    partial class Izmena_Milanka
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RepositoryCore/Migrations/20181018063235_Izmena_Milanka.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RepositoryCore.Migrations
+{
+    public partial class Izmena_Milanka : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IndustryCode",
+                table: "BusinessPartners");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IndustryCode",
+                table: "BusinessPartners",
+                nullable: true);
+        }
+    }
+}
