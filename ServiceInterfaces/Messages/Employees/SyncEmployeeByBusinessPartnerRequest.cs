ad6c252e400daff14755f239172d3223f560c299
Sudar88 <sasa.sudar@gmail.com>
2018-11-02 16:05:09 +0100

    ServiceInterface (EmployeeByBusinesPartner i BusinessPartnerHistory & BusinessPartnerByConstructionSite + ConstructionSiteHistory)

---
+++ ServiceInterfaces/Messages/Employees/SyncEmployeeByBusinessPartnerRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceInterfaces.Messages.Employees
+{
+    public class SyncEmployeeByBusinessPartnerRequest
+    {
+        public int CompanyId { get; set; }
+        public DateTime? LastUpdateAt { get; set; }
+    }
+}
