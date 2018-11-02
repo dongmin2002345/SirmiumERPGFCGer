b1862f5f3371ecd0ac103907bf30b38a5e39fb89
Sudar88 <sasa.sudar@gmail.com>
2018-11-02 16:33:12 +0100

    Maperi

---
+++ DataMapper/Mappers/Employees/EmployeeByBusinessPartnerHistoryMapper.cs
@@ -0,0 +1,95 @@
+﻿using DataMapper.Mappers.Common.BusinessPartners;
+using DataMapper.Mappers.Common.Companies;
+using DataMapper.Mappers.Common.Identity;
+using DomainCore.Employees;
+using ServiceInterfaces.ViewModels.Employees;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataMapper.Mappers.Employees
+{
+    public static class EmployeeByBusinessPartnerHistoryMapper
+    {
+        public static List<EmployeeByBusinessPartnerHistoryViewModel> ConvertToEmployeeByBusinessPartnerHistoryViewModelList(this IEnumerable<EmployeeByBusinessPartnerHistory> employeeByBusinessPartnerHistories)
+        {
+            List<EmployeeByBusinessPartnerHistoryViewModel> employeeByBusinessPartnerHistoryViewModels = new List<EmployeeByBusinessPartnerHistoryViewModel>();
+            foreach (EmployeeByBusinessPartnerHistory employeeByBusinessPartnerHistory in employeeByBusinessPartnerHistories)
+            {
+                employeeByBusinessPartnerHistoryViewModels.Add(employeeByBusinessPartnerHistory.ConvertToEmployeeByBusinessPartnerHistoryViewModel());
+            }
+            return employeeByBusinessPartnerHistoryViewModels;
+        }
+
+        public static EmployeeByBusinessPartnerHistoryViewModel ConvertToEmployeeByBusinessPartnerHistoryViewModel(this EmployeeByBusinessPartnerHistory employeeByBusinessPartnerHistory)
+        {
+            EmployeeByBusinessPartnerHistoryViewModel employeeByBusinessPartnerHistoryViewModel = new EmployeeByBusinessPartnerHistoryViewModel()
+            {
+                Id = employeeByBusinessPartnerHistory.Id,
+                Identifier = employeeByBusinessPartnerHistory.Identifier,
+
+                Code = employeeByBusinessPartnerHistory.Code,
+
+                StartDate = employeeByBusinessPartnerHistory.StartDate,
+                EndDate = employeeByBusinessPartnerHistory.EndDate,
+
+                Employee = employeeByBusinessPartnerHistory.Employee?.ConvertToEmployeeViewModelLite(),
+                BusinessPartner = employeeByBusinessPartnerHistory.BusinessPartner?.ConvertToBusinessPartnerViewModelLite(),
+
+                CreatedBy = employeeByBusinessPartnerHistory.CreatedBy?.ConvertToUserViewModelLite(),
+                Company = employeeByBusinessPartnerHistory.Company?.ConvertToCompanyViewModelLite(),
+
+                UpdatedAt = employeeByBusinessPartnerHistory.UpdatedAt,
+                CreatedAt = employeeByBusinessPartnerHistory.CreatedAt
+            };
+
+
+
+            return employeeByBusinessPartnerHistoryViewModel;
+        }
+
+        public static EmployeeByBusinessPartnerHistoryViewModel ConvertToEmployeeByBusinessPartnerHistoryViewModelLite(this EmployeeByBusinessPartnerHistory employeeByBusinessPartnerHistory)
+        {
+            EmployeeByBusinessPartnerHistoryViewModel employeeByBusinessPartnerHistoryViewModel = new EmployeeByBusinessPartnerHistoryViewModel()
+            {
+                Id = employeeByBusinessPartnerHistory.Id,
+                Identifier = employeeByBusinessPartnerHistory.Identifier,
+
+                Code = employeeByBusinessPartnerHistory.Code,
+
+                StartDate = employeeByBusinessPartnerHistory.StartDate,
+                EndDate = employeeByBusinessPartnerHistory.EndDate,
+
+                UpdatedAt = employeeByBusinessPartnerHistory.UpdatedAt,
+                CreatedAt = employeeByBusinessPartnerHistory.CreatedAt
+            };
+
+
+            return employeeByBusinessPartnerHistoryViewModel;
+        }
+
+        public static EmployeeByBusinessPartnerHistory ConvertToEmployeeByBusinessPartnerHistory(this EmployeeByBusinessPartnerHistoryViewModel employeeByBusinessPartnerHistoryViewModel)
+        {
+            EmployeeByBusinessPartnerHistory employeeByBusinessPartnerHistory = new EmployeeByBusinessPartnerHistory()
+            {
+                Id = employeeByBusinessPartnerHistoryViewModel.Id,
+                Identifier = employeeByBusinessPartnerHistoryViewModel.Identifier,
+
+                Code = employeeByBusinessPartnerHistoryViewModel.Code,
+                StartDate = employeeByBusinessPartnerHistoryViewModel.StartDate,
+                EndDate = employeeByBusinessPartnerHistoryViewModel.EndDate,
+
+                EmployeeId = employeeByBusinessPartnerHistoryViewModel.Employee?.Id ?? null,
+                BusinessPartnerId = employeeByBusinessPartnerHistoryViewModel.BusinessPartner?.Id ?? null,
+
+                CreatedById = employeeByBusinessPartnerHistoryViewModel.CreatedBy?.Id ?? null,
+                CompanyId = employeeByBusinessPartnerHistoryViewModel.Company?.Id ?? null,
+
+                CreatedAt = employeeByBusinessPartnerHistoryViewModel.CreatedAt,
+                UpdatedAt = employeeByBusinessPartnerHistoryViewModel.UpdatedAt
+            };
+
+            return employeeByBusinessPartnerHistory;
+        }
+    }
+}
---
+++ DataMapper/Mappers/Employees/EmployeeByBusinessPartnerMapper.cs
@@ -0,0 +1,92 @@
+﻿using DataMapper.Mappers.Common.BusinessPartners;
+using DataMapper.Mappers.Common.Companies;
+using DataMapper.Mappers.Common.Identity;
+using DomainCore.Employees;
+using ServiceInterfaces.ViewModels.Employees;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataMapper.Mappers.Employees
+{
+    public static class EmployeeByBusinessPartnerMapper
+    {
+        public static List<EmployeeByBusinessPartnerViewModel> ConvertToEmployeeByBusinessPartnerViewModelList(this IEnumerable<EmployeeByBusinessPartner> employeeByBusinessPartners)
+        {
+            List<EmployeeByBusinessPartnerViewModel> employeeByBusinessPartnerViewModels = new List<EmployeeByBusinessPartnerViewModel>();
+            foreach (EmployeeByBusinessPartner employeeByBusinessPartner in employeeByBusinessPartners)
+            {
+                employeeByBusinessPartnerViewModels.Add(employeeByBusinessPartner.ConvertToEmployeeByBusinessPartnerViewModel());
+            }
+            return employeeByBusinessPartnerViewModels;
+        }
+
+        public static EmployeeByBusinessPartnerViewModel ConvertToEmployeeByBusinessPartnerViewModel(this EmployeeByBusinessPartner employeeByBusinessPartner)
+        {
+            EmployeeByBusinessPartnerViewModel remedyViewModel = new EmployeeByBusinessPartnerViewModel()
+            {
+                Id = employeeByBusinessPartner.Id,
+                Identifier = employeeByBusinessPartner.Identifier,
+
+                Code = employeeByBusinessPartner.Code,
+
+                StartDate = employeeByBusinessPartner.StartDate,
+
+                Employee = employeeByBusinessPartner.Employee?.ConvertToEmployeeViewModelLite(),
+                BusinessPartner = employeeByBusinessPartner.BusinessPartner?.ConvertToBusinessPartnerViewModelLite(),
+
+                CreatedBy = employeeByBusinessPartner.CreatedBy?.ConvertToUserViewModelLite(),
+                Company = employeeByBusinessPartner.Company?.ConvertToCompanyViewModelLite(),
+
+                UpdatedAt = employeeByBusinessPartner.UpdatedAt,
+                CreatedAt = employeeByBusinessPartner.CreatedAt
+            };
+
+
+
+            return remedyViewModel;
+        }
+
+        public static EmployeeByBusinessPartnerViewModel ConvertToEmployeeByBusinessPartnerViewModelLite(this EmployeeByBusinessPartner employeeByBusinessPartner)
+        {
+            EmployeeByBusinessPartnerViewModel remedyViewModel = new EmployeeByBusinessPartnerViewModel()
+            {
+                Id = employeeByBusinessPartner.Id,
+                Identifier = employeeByBusinessPartner.Identifier,
+
+                Code = employeeByBusinessPartner.Code,
+
+                StartDate = employeeByBusinessPartner.StartDate,
+
+                UpdatedAt = employeeByBusinessPartner.UpdatedAt,
+                CreatedAt = employeeByBusinessPartner.CreatedAt
+            };
+
+
+            return remedyViewModel;
+        }
+
+        public static EmployeeByBusinessPartner ConvertToEmployeeByBusinessPartner(this EmployeeByBusinessPartnerViewModel employeeByBusinessPartnerViewModel)
+        {
+            EmployeeByBusinessPartner employeeByBusinessPartner = new EmployeeByBusinessPartner()
+            {
+                Id = employeeByBusinessPartnerViewModel.Id,
+                Identifier = employeeByBusinessPartnerViewModel.Identifier,
+
+                Code = employeeByBusinessPartnerViewModel.Code,
+                StartDate = employeeByBusinessPartnerViewModel.StartDate,
+
+                EmployeeId = employeeByBusinessPartnerViewModel.Employee?.Id ?? null,
+                BusinessPartnerId = employeeByBusinessPartnerViewModel.BusinessPartner?.Id ?? null,
+
+                CreatedById = employeeByBusinessPartnerViewModel.CreatedBy?.Id ?? null,
+                CompanyId = employeeByBusinessPartnerViewModel.Company?.Id ?? null,
+
+                CreatedAt = employeeByBusinessPartnerViewModel.CreatedAt,
+                UpdatedAt = employeeByBusinessPartnerViewModel.UpdatedAt
+            };
+
+            return employeeByBusinessPartner;
+        }
+    }
+}
