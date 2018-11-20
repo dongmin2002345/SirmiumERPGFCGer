07eba594db052dd1fec37a300f536581bccce0e4
Sudar88 <sasa.sudar@gmail.com>
2018-11-21 00:56:53 +0100

    manje izmene View

--- RepositoryCore/DbViews/Common/Invoices/InputInvoiceView.cs
+++ RepositoryCore/DbViews/Common/Invoices/InputInvoiceView.cs
@@ -21,12 +21,12 @@ namespace RepositoryCore.DbViews.Common.Invoices
                 "CREATE VIEW vInputInvoices AS " +
                 "SELECT inputInvoice.Id AS InputInvoiceId, inputInvoice.Identifier AS InputInvoiceIdentifier, inputInvoice.Code AS InputInvoiceCode, " +
                 "businessPartner.Id AS BusinessPartnerId, businessPartner.Identifier AS BusinessPartnerIdentifier, businessPartner.Code AS BusinessPartnerCode, businessPartner.Name AS BusinessPartnerName, " +
-                "inputInvoice.Supplier, inputInvoice.Address, inputInvoice.InvoiceNumber, inputInvoice.InvoiceDate, inputInvoice.AmountNet, inputInvoice.PDVPercent, inputInvoice.PDV, inputInvoice.AmountGross, inputInvoice.Currency, inputInvoice.DateOfPayment, inputInvoice.Status, inputInvoice.StatusDate, inputInvoice.Description, inputInvoice.Active AS Active, " +
+                "inputInvoice.Supplier, inputInvoice.Address, inputInvoice.InvoiceNumber, inputInvoice.InvoiceDate, inputInvoice.AmountNet, inputInvoice.PDVPercent, inputInvoice.PDV, inputInvoice.AmountGross, inputInvoice.Currency, inputInvoice.DateOfPaymet, inputInvoice.Status, inputInvoice.StatusDate, inputInvoice.Description, inputInvoice.Active AS Active, " +
                 "(SELECT MAX(v) FROM (VALUES (inputInvoice.UpdatedAt), (businessPartner.UpdatedAt)) AS value(v)) AS UpdatedAt, " +
                 "createdBy.Id AS CreatedById, createdBy.FirstName AS CreatedByFirstName, createdBy.LastName AS CreatedByLastName, " +
                 "company.Id AS CompanyId, company.Name AS CompanyName " +
                 "FROM InputInvoices inputInvoice " +
-                "LEFT JOIN BusinessPartner businessPartner ON inputInvoice.BusinessPartnerId = businessPartner.Id " +
+                "LEFT JOIN BusinessPartners businessPartner ON inputInvoice.BusinessPartnerId = businessPartner.Id " +
                 "LEFT JOIN Users createdBy ON inputInvoice.CreatedById = createdBy.Id " +
                 "LEFT JOIN Companies company ON inputInvoice.CompanyId = company.Id;";
 
