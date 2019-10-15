8792978380f205ce4d085210304e85b802b6d21c
Sudar88 <sasa.sudar@gmail.com>
2019-10-15 19:35:16 +0200

    izmene

--- SirmiumERPGFC/Views/Employees/LicenceType_List_AddEdit.xaml.cs
+++ SirmiumERPGFC/Views/Employees/LicenceType_List_AddEdit.xaml.cs
@@ -8,21 +8,10 @@ using SirmiumERPGFC.Common;
 using SirmiumERPGFC.Infrastructure;
 using SirmiumERPGFC.Repository.Employees;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SirmiumERPGFC.Views.Employees
 {
@@ -193,7 +182,6 @@ namespace SirmiumERPGFC.Views.Employees
 
 				if (response.Success)
 				{
-					new LicenceTypeSQLiteRepository().UpdateSyncStatus(response.LicenceType.Identifier, response.LicenceType.Code, response.LicenceType.UpdatedAt, response.LicenceType.Id, true);
 					MainWindow.SuccessMessage = ((string)Application.Current.FindResource("Podaci_su_uspešno_sačuvaniUzvičnik"));
                     SaveButtonContent = ((string)Application.Current.FindResource("Sačuvaj"));
                     SaveButtonEnabled = true;
