7ea703b7bc55de943437de3e36d7a1dcb815fa53
nemanja.04.07 <nemanja.04.07@gmail.com>
2019-10-18 11:36:27 +0200

    izmene

--- SirmiumERPGFC/Views/BusinessPartners/BusinessPartner_Phone_AddEdit.xaml.cs
+++ SirmiumERPGFC/Views/BusinessPartners/BusinessPartner_Phone_AddEdit.xaml.cs
@@ -415,7 +415,7 @@ namespace SirmiumERPGFC.Views.BusinessPartners
 
             fileDIalog.Multiselect = true;
             fileDIalog.FileOk += FileDIalog_FileOk;
-            fileDIalog.Filter = "Image files (*.jpg)|*.jpg|All Files (*.*)|*.*";
+            fileDIalog.Filter = "All Files (*.*)|*.*";
             fileDIalog.ShowDialog();
         }
 
