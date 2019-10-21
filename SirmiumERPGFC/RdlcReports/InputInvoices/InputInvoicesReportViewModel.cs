dd97f461010ec0572c38993b575b27bb9b8b08eb
nemanja.04.07 <nemanja.04.07@gmail.com>
2019-10-21 13:53:09 +0200

    sitne izmene

---
+++ SirmiumERPGFC/RdlcReports/InputInvoices/InputInvoicesReportViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SirmiumERPGFC.RdlcReports.InputInvoices
+{
+    public class InputInvoicesReportViewModel
+    {
+        public string BusinessPartnerName { get; set; }
+        public string Supplier { get; set; }
+        public string Address { get; set; }
+        public string InvoiceNumber { get; set; }
+        public string InvoiceDate { get; set; }
+        public string AmountNet { get; set; }
+        public string PDVPercent { get; set; }
+        public string PDV { get; set; }
+        public string AmountGross { get; set; }
+        public string Currency { get; set; }
+        public string DateOfPaymet { get; set; }
+        public string Status { get; set; }
+        public string StatusDate { get; set; }
+    }
+}
