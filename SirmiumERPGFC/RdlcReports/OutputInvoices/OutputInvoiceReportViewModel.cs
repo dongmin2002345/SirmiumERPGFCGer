a7c7204a681f0d70542d62810f31103ae9f625ea
nemanja.04.07 <nemanja.04.07@gmail.com>
2019-10-22 15:46:46 +0200

    sitne izmene

---
+++ SirmiumERPGFC/RdlcReports/OutputInvoices/OutputInvoiceReportViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SirmiumERPGFC.RdlcReports.OutputInvoices
+{
+    public class OutputInvoiceReportViewModel
+    {
+        public string OrderNumberOutputInvoice { get; set; }
+        public string BusinessPartnerName { get; set; }
+        public string Supplier { get; set; }
+        public string Address { get; set; }
+        public string InvoiceNumber { get; set; }
+        public string InvoiceDate { get; set; }
+        public string AmountNet { get; set; }
+        public string PDVPercent { get; set; }
+        public string PDV { get; set; }
+        public string AmountGross { get; set; }
+        public string Currency { get; set; }
+        public string DateOfPaymet { get; set; }
+        public string Status { get; set; }
+        public string StatusDate { get; set; }
+    }
+}
