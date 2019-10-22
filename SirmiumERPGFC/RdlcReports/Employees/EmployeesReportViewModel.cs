20f7510c357bd58393748dc0076d82126d9d2a38
nemanja.04.07 <nemanja.04.07@gmail.com>
2019-10-22 18:50:28 +0200

    izmene

---
+++ SirmiumERPGFC/RdlcReports/Employees/EmployeesReportViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SirmiumERPGFC.RdlcReports.Employees
+{
+    public class EmployeesReportViewModel
+    {
+        public string OrderNumbersForEmployees { get; set; }
+        public string EmployeeCode { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string ConstructionSiteCode { get; set; }
+        public string ConstructionSiteName { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Passport { get; set; }
+        public string ResidenceCountryName { get; set; }
+        public string ResidenceCityName { get; set; }
+        public string ResidenceAddress { get; set; }
+        
+    }
+}
