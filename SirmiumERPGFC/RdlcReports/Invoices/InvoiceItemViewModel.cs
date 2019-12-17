f66af950cfe53e2b31255367658aabd80ecb2453
ssone95 <nshone95@hotmail.com>
2019-12-17 12:16:16 +0100

    RDLC i datasetovi

---
+++ SirmiumERPGFC/RdlcReports/Invoices/InvoiceItemViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SirmiumERPGFC.RdlcReports.Invoices
+{
+    public class InvoiceItemViewModel
+    {
+        public string Id { get; set; }
+        public string ProductCode{ get; set; }
+        public string ProductName{ get; set; }
+        public string UnitOfMeasurement{ get; set; }
+        public string Quantity{ get; set; }
+        public string PriceWithoutPDV{ get; set; }
+        public string PriceWithPDV{ get; set; }
+        public string Rebate { get; set; }
+        public string PDVPercent{ get; set; }
+        public string PDVValue{ get; set; }
+        public string Amount { get; set; }
+    }
+}
---
+++ SirmiumERPGFC/RdlcReports/Invoices/InvoiceViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SirmiumERPGFC.RdlcReports.Invoices
+{
+    public class InvoiceViewModel
+    {
+        public string InvoiceNumber { get; set; }
+        public string CityAndInvoiceDate { get; set; }
+        public string DeliveryDateOfGoodsAndServices { get; set; }
+        public string DueDate { get; set; }
+
+        public string BusinessPartnerCode { get; set; }
+        public string BusinessPartnerPIB { get; set; }
+        public string BusinessPartnerMB { get; set; }
+        public string BusinessPartnerName { get; set; }
+        public string BusinessPartnerAddress { get; set; }
+        public string BusinessPartnerCity { get; set; }
+
+        public string Amount { get; set; }
+
+        public string TotalBase { get; set; }
+        public string TotalPDV { get; set; }
+        public string AmountInCurrency { get; set; }
+        
+        public string TotalAmount { get; set; }
+        
+        public string TotalAmountInCurrency { get; set; }
+    }
+}
