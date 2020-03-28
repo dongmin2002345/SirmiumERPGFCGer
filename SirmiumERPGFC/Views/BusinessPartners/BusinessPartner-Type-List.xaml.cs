d35b76f8a01c5124db5cc676570a134f09bb8fec
Zdravko Ivankovic <ivankovic.zdravko@gmail.com>
2020-03-28 11:52:49 +0100

    Razne izmene

--- SirmiumERPGFC/Views/BusinessPartners/BusinessPartner-Type-List.xaml.cs
+++ SirmiumERPGFC/Views/BusinessPartners/BusinessPartner-Type-List.xaml.cs
@@ -6,24 +6,13 @@ using ServiceInterfaces.ViewModels.Common.BusinessPartners;
 using SirmiumERPGFC.Common;
 using SirmiumERPGFC.Infrastructure;
 using SirmiumERPGFC.Repository.BusinessPartners;
-using SirmiumERPGFC.Views.Common;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SirmiumERPGFC.Views.BusinessPartners
 {
@@ -254,6 +243,11 @@ namespace SirmiumERPGFC.Views.BusinessPartners
 
             RefreshButtonContent = ((string)Application.Current.FindResource("OSVEŽI"));
             RefreshButtonEnabled = true;
+        }
+
+        private void dgBusinessPartnerTypes_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = (e.Row.GetIndex() + 1).ToString();
         }
 
         #endregion
--- SirmiumERPGFC/Views/Sectors/AgencyList.xaml.cs
+++ SirmiumERPGFC/Views/Sectors/AgencyList.xaml.cs
@@ -5,24 +5,13 @@ using ServiceInterfaces.ViewModels.Common.Sectors;
 using SirmiumERPGFC.Common;
 using SirmiumERPGFC.Infrastructure;
 using SirmiumERPGFC.Repository.Sectors;
-using SirmiumERPGFC.Views.Common;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SirmiumERPGFC.Views.Sectors
 {
@@ -248,6 +237,11 @@ namespace SirmiumERPGFC.Views.Sectors
 
             RefreshButtonContent = ((string)Application.Current.FindResource("OSVEŽI"));
             RefreshButtonEnabled = true;
+        }
+
+        private void dgAgencies_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = (e.Row.GetIndex() + 1).ToString();
         }
 
         #endregion
