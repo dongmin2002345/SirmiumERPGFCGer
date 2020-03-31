10ef34a094b2d5eb9ec3726b6ac90aa822ee099b
nemanja.04.07 <nemanja.04.07@gmail.com>
2020-03-31 11:29:16 +0200

    Izmene + MIGRACIJA

--- DomainCore/Common/BusinessPartners/BusinessPartner.cs
+++ DomainCore/Common/BusinessPartners/BusinessPartner.cs
@@ -58,6 +58,12 @@ namespace DomainCore.Common.BusinessPartners
 
         public int? CountryId { get; set; }
         public Country Country { get; set; }
+
+        public int? CityId { get; set; }
+        public City City { get; set; }
+
+        public string AddressGer { get; set; }
+
         public int? SectorId { get; set; }
         public Sector Sector { get; set; }
         public int? AgencyId { get; set; }
--- RepositoryCore/DbViews/Common/BusinessPartners/BusinessPartnerView.cs
+++ RepositoryCore/DbViews/Common/BusinessPartners/BusinessPartnerView.cs
@@ -31,13 +31,17 @@ namespace RepositoryCore.DbViews.Common.BusinessPartners
                 "taxAdministration.Id AS TaxAdministrationId, taxAdministration.Identifier AS TaxAdministrationIdentifier, taxAdministration.Code AS TaxAdministrationCode, taxAdministration.Name AS TaxAdministrationName, " +
                 "businessPartner.IBAN, businessPartner.BetriebsNumber, businessPartner.Customer, " +
                 "country.Id AS CountryId, country.Identifier AS CountryIdentifier, country.Code AS CountryCode, country.Name AS CountryName, " +
+
+                "city.Id AS CityId, city.Identifier AS CityIdentifier, city.ZipCode AS CityZipCode, city.Name AS CityName, " +
+                "businessPartner.AddressGer, " +
+
                 "sector.Id AS SectorId, sector.Identifier AS SectorIdentifier, sector.Code AS SectorCode, sector.Name AS SectorName, " +
                 "agency.Id AS AgencyId, agency.Identifier AS AgencyIdentifier, agency.Code AS AgencyCode, agency.Name AS AgencyName, " +
                 "vat.Id AS VatId, vat.Identifier AS VatIdentifier, vat.Code AS VatCode, vat.Description AS VatDescription, vat.Amount AS VatAmount, " +
                 "discount.Id AS DiscountId, discount.Identifier AS DiscountIdentifier, discount.Code AS DiscountCode, discount.Name AS DiscountName, discount.Amount AS DiscountAmount, " +
                 "businessPartner.TaxNr, businessPartner.CommercialNr, businessPartner.ContactPersonGer, businessPartner.VatDeductionFrom, businessPartner.Path, businessPartner.VatDeductionTo,  businessPartner.PdvType as PdvType, businessPartner.Active AS Active, " +
 
-                "(SELECT MAX(v) FROM (VALUES (businessPartner.UpdatedAt), (taxAdministration.UpdatedAt), (country.UpdatedAt), (sector.UpdatedAt), (agency.UpdatedAt), (countrySrb.UpdatedAt), (citySrb.UpdatedAt)) AS value(v)) AS UpdatedAt, " +
+                "(SELECT MAX(v) FROM (VALUES (businessPartner.UpdatedAt), (taxAdministration.UpdatedAt), (country.UpdatedAt), (sector.UpdatedAt), (agency.UpdatedAt), (countrySrb.UpdatedAt), (citySrb.UpdatedAt), (city.UpdatedAt)) AS value(v)) AS UpdatedAt, " +
 
                 "createdBy.Id AS CreatedById, createdBy.FirstName AS CreatedByFirstName, createdBy.LastName AS CreatedByLastName, " +
                 "company.Id AS CompanyId, company.Name AS CompanyName " +
@@ -47,6 +51,7 @@ namespace RepositoryCore.DbViews.Common.BusinessPartners
                 "LEFT JOIN Countries country ON businessPartner.CountryId = country.Id " +
                 "LEFT JOIN Countries countrySrb ON businessPartner.CountrySrbId = countrySrb.Id " +
                 "LEFT JOIN Cities citySrb ON businessPartner.CitySrbId = citySrb.Id " +
+                "LEFT JOIN Cities city ON businessPartner.CityId = city.Id " +
                 "LEFT JOIN Sectors sector ON businessPartner.SectorId = sector.Id " +
                 "LEFT JOIN Agencies agency ON businessPartner.AgencyId = agency.Id " +
                 "LEFT JOIN Vats vat ON businessPartner.VatId = vat.Id " +
